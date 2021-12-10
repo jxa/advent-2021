a0ed991361e7540c1134b615b8f299f293a60ca3
John Andrews <john.m.andrews@gmail.com>
2021-12-09 20:43:14 -0500

    Day 9 - Smoke Basin

--- src/xandrews/day09.clj
+++ src/xandrews/day09.clj
@@ -1,7 +1,9 @@
 (ns xandrews.day09
   (:require [clojure.string :as s]
             [clojure.core.matrix :as matrix]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [ubergraph.core :as uber]
+            [ubergraph.alg :refer [connected-components]]))
 
 ;; # Day 9: Smoke Basin
 
@@ -95,3 +97,99 @@
 ;; levels of all low points on your heightmap?
 
 (reduce + (map risk (low-points (parse-heights (slurp (io/resource "day09.txt"))))))
+
+;; # Part Two
+
+;; Next, you need to find the largest basins so you know what areas are most
+;; important to avoid.
+
+;; A basin is all locations that eventually flow downward to a single low point.
+;; Therefore, every low point has a basin, although some basins are very small.
+;; Locations of height 9 do not count as being in any basin, and all other
+;; locations will always be part of exactly one basin.
+
+(defn describe-coordinates
+  [m x y]
+  (when-let [point (safe-select-point m x y)]
+    {:point point
+     :coordinates [x y]}))
+
+(defn connections-seq [m]
+  (map (fn [[x y]]
+         (assoc (describe-coordinates m x y)
+                :connected (filterv (complement nil?)
+                             [(describe-coordinates m x (inc y))
+                              (describe-coordinates m x (dec y))
+                              (describe-coordinates m (inc x) y)
+                              (describe-coordinates m (dec x) y)])))
+       (matrix/index-seq m)))
+
+(connections-seq test-heights)
+
+
+(defn node-edges
+  "A node's edges will point 'uphill'"
+  [node]
+  (->> (:connected node)
+       (map (fn [n]
+              (when (and (not= 9 (:point n))
+                         (> (:point n) (:point node)))
+                [(:coordinates node) (:coordinates n)])))
+       (filter seq)))
+
+(let [nodes (connections-seq test-heights)
+      n1 (first nodes)
+      n2 (nth nodes 1)]
+  {1 (node-edges n1)
+   2 (node-edges n2)})
+
+(defn basin-graph
+  [m]
+  (apply uber/digraph
+         (reduce (fn [edges node]
+                   (if (= 9 (:point node))
+                     edges
+                     (let [es (node-edges node)]
+                       (if (seq es)
+                         (into edges es)
+                         edges))))
+                 []
+                 (connections-seq m))))
+
+(with-out-str
+  (uber/pprint
+   (basin-graph test-heights)))
+
+
+;; The size of a basin is the number of locations within the basin, including
+;; the low point. The example above has four basins.
+
+;;     The top-left basin, size 3:
+;;     The top-right basin, size 9:
+;;     The middle basin, size 14:
+;;     The bottom-right basin, size 9:
+;; (see [original problem description](https://adventofcode.com/2021/day/9#part2) for highlighted version)
+
+;; Find the three largest basins and multiply their sizes together. In the above
+;; example, this is 9 * 14 * 9 = 1134.
+
+(reduce *
+        (map count
+             (take 3
+                   (sort-by (comp - count)
+                            (connected-components
+                             (basin-graph test-heights))))))
+
+
+;; What do you get if you multiply together the sizes of the three largest
+;; basins?
+
+(->> (io/resource "day09.txt")
+     slurp
+     parse-heights
+     basin-graph
+     connected-components
+     (sort-by (comp - count)) ;; sort by descending size
+     (take 3)
+     (map count)
+     (reduce *))
